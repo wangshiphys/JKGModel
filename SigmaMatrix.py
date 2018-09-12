68b93fda905b39862ee15e1102143cd8533f4416
wangshiphys <wangshiphys@gmail.com>
2018-09-12 14:25:30 +0800

    Delete these statements that set the sigma matrices to be read only

--- SigmaMatrix.py
+++ SigmaMatrix.py
@@ -13,16 +13,8 @@ SIGMAY = np.array([[0, -1j], [1j, 0]], dtype=np.complex128)
 SIGMAZ = np.array([[1, 0], [0, -1]], dtype=np.float64)
 SIGMAS = np.array([SIGMA0, SIGMAX, SIGMAY, SIGMAZ])
 
-SIGMA0.setflags(write=False)
-SIGMAX.setflags(write=False)
-SIGMAY.setflags(write=False)
-SIGMAZ.setflags(write=False)
-SIGMAS.setflags(write=False)
-
 # sigmay * 1j
 ISIGMAY = np.array([[0, 1], [-1, 0]], dtype=np.float64)
-ISIGMAY.setflags(write=False)
 
 # pairing matrices
 PMS = np.matmul(ISIGMAY, SIGMAS) / np.sqrt(2)
-PMS.setflags(write=False)
