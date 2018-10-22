1f8a6f3126b55d8a0ef0698875afa41ffeaabbfa
wangshiphys <wangshiphys@gmail.com>
2018-10-22 21:36:29 +0800

    Add a tool script `ExtractGE.py` for retrieving ground state energies from the stored ground state data

---
+++ ExtractGE.py
@@ -0,0 +1,26 @@
+"""
+Extract the ground state energies from the stored ground state data
+"""
+
+
+from pathlib import Path
+from time import time
+
+import numpy as np
+
+
+gs_dir = Path("data/SpinModel/GS/")
+
+info = "The time spend on processing {0} is: {1:.6f}s"
+
+for sub_dir in gs_dir.iterdir():
+    Path(str(sub_dir).replace("GS", "GE")).mkdir(parents=True, exist_ok=True)
+    for gs_file in sub_dir.iterdir():
+        t0 = time()
+        ge_file = str(gs_file).replace("GS", "GE").replace("npz", "npy")
+        with np.load(gs_file) as ld:
+            alpha, beta = ld["parameters"]
+            gse = ld["gse"][0]
+        np.save(ge_file, [alpha, beta, gse])
+        t1 = time()
+        print(info.format(gs_file.name, t1 - t0))
