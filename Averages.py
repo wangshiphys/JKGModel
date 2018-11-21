8fb0ca9e0a6135385a8ce57b2bf1d144f7c62d12
wangshiphys <wangshiphys@gmail.com>
2018-11-21 13:02:48 +0800

    Delete the TotalSpin.py, part of the function of this module is now integrated to the Averages.py module; A new algorithm is used to calculate the ground state averages, the new implementation of the Averages module is more efficient and require less memory.

--- Averages.py
+++ Averages.py
@@ -4,83 +4,77 @@ Calculate the ground state averages of spin operators of the system
 
 
 from datetime import datetime
-from multiprocessing import current_process, Process, Queue
 from pathlib import Path
 from time import time
 
-from scipy.sparse import load_npz
+from scipy.sparse import identity, kron
 
-import mkl
 import numpy as np
 
 
-mkl.set_num_threads(1)
+__all__ = [
+    "TotalSpin",
+    "GSAverages",
+]
 
-LOG_FMT = "{now:%Y-%m-%d %H:%M:%S} - {process} - {message}\n"
+
+SPIN_X_MATRICES = np.array([[0.0, 0.5], [0.5, 0.0]], dtype=np.float64)
+# The real SY matrix is np.array([[0.0, -0.5j], [0.5j, 0.0]])
+# The following  matrix is the imaginary part
+SPIN_Y_IMAG_MATRICES = np.array([[0.0, -0.5], [0.5, 0.0]], dtype=np.float64)
+SPIN_Z_MATRICES = np.array([[0.5, 0.0], [0.0, -0.5]], dtype=np.float64)
 
 
-def LoadGS(params, process_num, GSQueue, log_queue, numx=4, numy=6):
+LOG_TEMPLATE = "{now:%Y-%m-%d %H:%M:%S} - {message} - Time Used: {dt:.3f}s"
+
+gs_path_template = "data/SpinModel/GS/alpha={alpha:.3f}/"
+avg_path_template = "data/SpinModel/Averages/alpha={alpha:.3f}/"
+gs_name_template = "GS_numx={numx}_numy={numy}_" \
+                   "alpha={alpha:.3f}_beta={beta:.3f}.npz"
+avg_name_template = "Averages_numx={numx}_numy={numy}_" \
+                   "alpha={alpha:.3f}_beta={beta:.3f}.npz"
+
+
+def TotalSpin(spin_num):
     """
-    Load the ground state vector
+    Calculate the total spin matrices of a system with N spins
 
     Parameters
-    ----------
-    params : sequence
-        A collection of (alpha, beta), which specifies the model parameter
-    process_num : int
-        The number of process used to calculate the ground state averages
-    GSQueue : multiprocessing.Queue
-        A queue of ground state vectors waiting to be processed
-    log_queue : multiprocessing.Queue
-        A queue of log message
-    numx : int, optional
-        The number of lattice site along the first translation vector
-        default: 4
-    numy : int, optional
-        The number of lattice site along the second translation vector
-        default: 6
+    spin_num : int
+        The number of spin in the system
+
+    Returns
+    -------
+    total_sx : csr_matrix
+        The total S^x matrix
+    total_sy_imag : csr_matrix
+        The imaginary part of the total S^y matrix
+        Since the S^y matrix is pure imaginary, so total_sy = 1j * total_sy_imag
+    total_sz : csr_matrix
+        The total S^z matrix
     """
 
-    gs_path_template = "data/SpinModel/GS/alpha={alpha:.3f}/"
-    gs_name_template = "GS_numx={0}_numy={1}".format(numx, numy)
-    gs_name_template += "_alpha={alpha:.3f}_beta={beta:.3f}.npz"
-    gs_full_name_template = gs_path_template + gs_name_template
+    assert isinstance(spin_num, int) and spin_num > 0
 
-    log_msg_template = "alpha={0:.3f}, beta={1:.3f} ready - Time used: {2:.3f}s"
-
-    process = current_process()
-    for alpha, beta in params:
-        t0 = time()
-        gs_full_name = gs_full_name_template.format(alpha=alpha, beta=beta)
-        with np.load(gs_full_name) as ld:
-            ket = ld["ket"]
-        t1 = time()
-
-        GSQueue.put((alpha, beta, ket))
-        log = LOG_FMT.format(
-            now=datetime.now(), process=process.name,
-            message=log_msg_template.format(alpha, beta, t1 - t0)
-        )
-        log_queue.put(log)
-
-    for i in range(process_num):
-        GSQueue.put(None)
-
-    log_queue.put(process.pid)
-    GSQueue.close()
-    log_queue.close()
+    total_sx = total_sy_imag = total_sz = 0.0
+    for index in range(spin_num):
+        I0 = identity(1 << index, np.float64, "csr")
+        I1 = identity(1 << (spin_num-index-1), np.float64, "csr")
+        total_sx += kron(I1, kron(SPIN_X_MATRICES, I0, "csr"), "csr")
+        total_sz += kron(I1, kron(SPIN_Z_MATRICES, I0, "csr"), "csr")
+        total_sy_imag += kron(I1, kron(SPIN_Y_IMAG_MATRICES, I0, "csr"), "csr")
+        del I0, I1
+    return total_sx, total_sy_imag, total_sz
 
 
-def GSAverages(GSQueue, log_queue, numx=4, numy=6):
+def GSAverages(params, numx=4, numy=6):
     """
     Calculate the ground state averages of spin operators of the system
 
     Parameters
     ----------
-    GSQueue : multiprocessing.Queue
-        A queue of ground state vectors waiting to be processed
-    log_queue : multiprocessing.Queue
-        A queue of log message
+    params : sequence
+        A collection of (alpha, beta), which specifies the model parameter
     numx : int, optional
         The number of lattice site along the first translation vector
         default: 4
@@ -89,122 +83,59 @@ def GSAverages(GSQueue, log_queue, numx=4, numy=6):
         default: 6
     """
 
-    avg_path_template = "data/SpinModel/Averages/alpha={alpha:.3f}/"
-    avg_name_template = "Averages_numx={0}_numy={1}".format(numx, numy)
-    avg_name_template += "_alpha={alpha:.3f}_beta={beta:.3f}.npz"
-
-    log_msg_template = "alpha={0:.3f}, beta={1:.3f} done  - Time used: {2:.3f}s"
-
-    SMatrices_name_template = "tmp/Total_{{0}} with spin_number={0}.npz".format(
-        numx * numy
+    t0 = time()
+    total_sx, total_sy_imag, total_sz = TotalSpin(numx * numy)
+    t1 = time()
+    log = LOG_TEMPLATE.format(
+        now=datetime.now(), message="Calculating Spin Matrices", dt=t1 - t0
     )
-    SX = load_npz(SMatrices_name_template.format("SX"))
-    SY = load_npz(SMatrices_name_template.format("SY"))
-    SZ = load_npz(SMatrices_name_template.format("SZ"))
-    S2 = load_npz(SMatrices_name_template.format("S2"))
+    print(log, flush=True)
 
-    process = current_process()
-    while True:
+    for alpha, beta in params:
         t0 = time()
-        task = GSQueue.get()
-        if task is None:
-            break
-        alpha, beta, ket = task
+        gs_full_name = (gs_path_template + gs_name_template).format(
+            numx=numx, numy=numy, alpha=alpha, beta=beta
+        )
+        with np.load(gs_full_name) as ld:
+            ket = ld["ket"]
 
-        SX_avg = np.vdot(ket, SX.dot(ket))
-        SY_avg = np.vdot(ket, SY.dot(ket))
-        SZ_avg = np.vdot(ket, SZ.dot(ket))
-        S2_avg = np.vdot(ket, S2.dot(ket))
+        total_sx_dot_ket = total_sx.dot(ket)
+        total_sy_imag_dot_ket = total_sy_imag.dot(ket)
+        total_sz_dot_ket = total_sz.dot(ket)
+
+        total_sx_avg = np.vdot(ket, total_sx_dot_ket)
+        total_sz_avg = np.vdot(ket, total_sz_dot_ket)
+        total_sy_avg = 1j * np.vdot(ket, total_sy_imag_dot_ket)
+        del ket
+
+        total_s2_avg = np.vdot(total_sy_imag_dot_ket, total_sy_imag_dot_ket)
+        del total_sy_imag_dot_ket
+        total_s2_avg += np.vdot(total_sx_dot_ket, total_sx_dot_ket)
+        del total_sx_dot_ket
+        total_s2_avg += np.vdot(total_sz_dot_ket, total_sz_dot_ket)
+        del total_sz_dot_ket
 
         avg_path = Path(avg_path_template.format(alpha=alpha))
         avg_path.mkdir(parents=True, exist_ok=True)
+        avg_full_name = avg_path / avg_name_template.format(
+            numx=numx, numy=numy, alpha=alpha, beta=beta
+        )
         np.savez(
-            avg_path / avg_name_template.format(alpha=alpha, beta=beta),
-            SX=[SX_avg], SY=[SY_avg], SZ=[SZ_avg], S2=[S2_avg]
+            avg_full_name,
+            total_sx_avg=[total_sx_avg], total_sy_avg=[total_sy_avg],
+            total_sz_avg=[total_sz_avg], total_s2_avg=[total_s2_avg],
         )
         t1 = time()
 
-        log = LOG_FMT.format(
-            now=datetime.now(), process=process.name,
-            message=log_msg_template.format(alpha, beta, t1 - t0)
+        log = LOG_TEMPLATE.format(
+            now=datetime.now(), dt= t1 - t0,
+            message="alpha={0:.3f}, beta={1:.3f} done".format(alpha, beta)
         )
-        log_queue.put(log)
-
-    log_queue.put(process.pid)
-    log_queue.close()
-
-
-def main(params, process_num=2, numx=4, numy=6):
-    """
-    The entrance for calculating the ground state averages
-
-    Parameters
-    ----------
-    params : sequence
-        A collection of (alpha, beta), which specifies the model parameter
-    process_num : int, optional
-        The number of process used to calculate the ground state averages
-        default: 2
-    numx : int, optional
-        The number of lattice site along the first translation vector
-        default: 4
-    numy : int, optional
-        The number of lattice site along the second translation vector
-        default: 6
-    """
-
-    assert isinstance(process_num, int) and process_num > 0
-
-    log_path = Path("log/SpinModel/")
-    log_path.mkdir(parents=True, exist_ok=True)
-
-    GSQueue = Queue(8 * process_num)
-    log_queue = Queue()
-
-    process_table = {}
-    p = Process(
-        target=LoadGS,
-        args=(params, process_num, GSQueue, log_queue, numx, numy)
-    )
-    p.start()
-    process_table[p.pid] = p
-
-    for i in range(process_num):
-        p = Process(
-            target=GSAverages,
-            args=(GSQueue, log_queue, numx, numy)
-        )
-        p.start()
-        process_table[p.pid] = p
-
-    with open(log_path / "Averages.log", mode="a", buffering=1) as fp:
-        log = LOG_FMT.format(
-            now=datetime.now(), message="Start running", process="Main"
-        )
-        fp.write(log)
-        while True:
-            log = log_queue.get()
-            if log in process_table:
-                new_log = LOG_FMT.format(
-                    now=datetime.now(), message="Finished",
-                    process=process_table[log].name,
-                )
-                fp.write(new_log)
-                process_table[log].join()
-                del process_table[log]
-            else:
-                fp.write(log)
-            if len(process_table) == 0:
-                break
-
-        log = LOG_FMT.format(
-            now=datetime.now(), message="Stop running", process="Main"
-        )
-        fp.write(log)
+        print(log, flush=True)
 
 
 if __name__ == "__main__":
-    alpha = 0.01
-    process_num = 3
-    params = [[alpha, beta] for beta in np.arange(0, 0.1, 0.01)]
-    main(params=params, process_num=process_num)
+    alpha = 0.50
+    betas = [1.45, 1.46, 1.47, 1.50, 1.51, 1.52, 1.53, 1.54, 1.55]
+    params = [[alpha, beta] for beta in betas]
+    GSAverages(params)
