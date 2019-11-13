a569d5eab6e72784ce2d0e9711f5cef94d19fb96
wangshiphys <wangshiphys@gmail.com>
2019-11-13 19:35:23 +0800

    Move the `ClassicalModel.py` from `classical` subpackage to root package; Change the classname from `ClassicalJKGGPModel` to `JKGModelClassicalSovler`.

--- ClassicalModel.py
+++ ClassicalModel.py
@@ -1,207 +1,133 @@
 """
-Analysing the classical J-K-Gamma-Gamma'(J-K-G-GP) model.
+Analysing the classical J-K-Gamma-Gamma' (J-K-G-GP) model.
 
 The spins are viewed as unit-vectors in 3-dimension.
 """
 
 
+import warnings
+from pathlib import Path
+
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.optimize import minimize
+import tables as tb
+from scipy.optimize import basinhopping
 
-from cluster import TriangularCluster
+from utilities import *
 
 
-class ClassicalJKGGPModel(TriangularCluster):
-    def _HijGenerator(self, model_params):
-        actual_model_params = dict(self.DEFAULT_MODEL_PARAMETERS)
-        actual_model_params.update(model_params)
-        J = actual_model_params["J"]
-        K = actual_model_params["K"]
-        G = actual_model_params["G"]
-        GP = actual_model_params["GP"]
+__all__ = [
+    "JKGModelClassicalSolver",
+]
 
-        Hijx = np.array(
-            [[J + K, GP, GP], [GP, J, G], [GP, G, J]], dtype=np.float64
+warnings.filterwarnings("ignore", category=tb.NaturalNameWarning)
+
+
+# Calculate the energy of the classical J-K-G-GP model with the given
+# spin-vectors. The spin-vectors are specified by the `spin_angles` parameters.
+def _EnergyForGeneralSpinConfig(spin_angles, all_bonds, all_Hs):
+    phis = spin_angles[0::2]
+    thetas = spin_angles[1::2]
+    sin_phis = np.sin(phis)
+    cos_phis = np.cos(phis)
+    sin_thetas = np.sin(thetas)
+    cos_thetas = np.cos(thetas)
+    vectors = np.array(
+        [sin_phis * sin_thetas, sin_phis * cos_thetas, cos_phis],
+        dtype=np.float64
+    )
+
+    energy = 0.0
+    for bonds, H in zip(all_bonds, all_Hs):
+        tmp = vectors[:, bonds]
+        energy += np.sum(tmp[:, :, 0] * np.dot(H, tmp[:, :, 1]))
+    return energy
+
+
+# Calculate the energy of the classical J-K-G-GP model with the given
+# spin-vectors. FM order is assumed.
+def _EnergyForFMSpinConfig(spin_angle, H):
+    phi = spin_angle[0]
+    theta = spin_angle[1]
+    sin_phi = np.sin(phi)
+    cos_phi = np.cos(phi)
+    sin_theta = np.sin(theta)
+    cos_theta = np.cos(theta)
+    vector = [sin_phi * sin_theta, sin_phi * cos_theta, cos_phi]
+    return np.dot(vector, np.dot(H, vector))
+
+
+class JKGModelClassicalSolver(TriangularLattice):
+    def MinimizeGeneralSpinConfig(
+            self, data_path="data/ClassicalSpinModel/OptimizedSpinConfig/",
+            alpha=0.5, beta=1.5, niter=200, show=True, **kwargs,
+    ):
+        J = np.sin(alpha * np.pi) * np.sin(beta * np.pi)
+        K = np.sin(alpha * np.pi) * np.cos(beta * np.pi)
+        G = np.cos(alpha * np.pi)
+        GP = 0.0
+
+        Hijx = np.array([[J + K, GP, GP], [GP, J, G], [GP, G, J]])
+        Hijy = np.array([[J, GP, G], [GP, J + K, GP], [G, GP, J]])
+        Hijz = np.array([[J, G, GP], [G, J, GP], [GP, GP, J + K]])
+        args = (self.all_bonds, (Hijx, Hijy, Hijz))
+
+        initial_spin_angles = np.pi * np.random.random(2 * self.site_num)
+        initial_spin_angles[1::2] *= 2
+        res = basinhopping(
+            _EnergyForGeneralSpinConfig, initial_spin_angles,
+            niter=niter, T=1.0, stepsize=0.5, minimizer_kwargs={"args": args},
         )
-        Hijy = np.array(
-            [[J, GP, G], [GP, J + K, GP], [G, GP, J]], dtype=np.float64
-        )
-        Hijz = np.array(
-            [[J, G, GP], [G, J, GP], [GP, GP, J + K]], dtype=np.float64
-        )
-        return Hijx, Hijy, Hijz
-
-    def GeneralSpinConfig(self, spin_angles, model_params):
-        phis = np.pi * spin_angles[0::2]
-        thetas = 2 * np.pi * spin_angles[1::2]
+        phis = res.x[0::2]
+        thetas = res.x[1::2]
         sin_phis = np.sin(phis)
         cos_phis = np.cos(phis)
         sin_thetas = np.sin(thetas)
         cos_thetas = np.cos(thetas)
-        spin_vectors = np.stack(
-            [sin_phis * sin_thetas, sin_phis * cos_thetas, cos_phis]
+        optimized_spin_vectors = np.array(
+            [sin_phis * sin_thetas, sin_phis * cos_thetas, cos_phis],
+            dtype=np.float64
         ).T
+        energy_per_site = res.fun / self._site_num
 
-        energy = 0.0
-        Hijx, Hijy, Hijz = self._HijGenerator(model_params)
-        for bonds, Hij in zip(self.all_bonds, [Hijx, Hijy, Hijz]):
-            bonds = np.array(bonds)
-            spins0 = spin_vectors[bonds[:, 0]]
-            spins1 = spin_vectors[bonds[:, 1]]
-            energy += np.sum(spins0 * np.dot(spins1, Hij))
-        return energy
-
-    def FMConfig(self, spin_angle, model_params):
-        phi = np.pi * spin_angle[0]
-        theta = 2 * np.pi * spin_angle[1]
-        sin_phi = np.sin(phi)
-        cos_phi = np.cos(phi)
-        sin_theta = np.sin(theta)
-        cos_theta = np.cos(theta)
-        spin_vector = [sin_phi * sin_theta, sin_phi * cos_theta, cos_phi]
-
-        x_bond_num = len(self._x_bonds)
-        y_bond_num = len(self._y_bonds)
-        z_bond_num = len(self._z_bonds)
-        Hijx, Hijy, Hijz = self._HijGenerator(model_params)
-        H = x_bond_num * Hijx + y_bond_num * Hijy + z_bond_num * Hijz
-
-        return np.dot(spin_vector, np.dot(H, spin_vector))
-
-    def Show3DVectorField(self, vectors, title=""):
-        points = np.zeros((self.site_num, 3), dtype=np.float64)
-        points[:, 0] = self.cluster.points[:, 0]
-        points[:, 1] = self.cluster.points[:, 1]
-
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection="3d")
-        ax.plot(
-            points[:, 0], points[:, 1], points[:, 2],
-            ls="", marker="o", color="k", ms=10,
+        Path(data_path).mkdir(exist_ok=True, parents=True)
+        h5f_name = "OSC_numx={0}_numy={1}_alpha={2:.4f}_beta={3:.4f}.h5".format(
+            self.numx, self.numy, alpha, beta
         )
-        ax.quiver(
-            points[:, 0], points[:, 1], points[:, 2],
-            vectors[:, 0], vectors[:, 1], vectors[:, 2],
-            length=0.5, arrow_length_ratio=0.5, pivot="middle",
-        )
-        ax.set_zlim(-0.5, 0.5)
-        ax.set_title(title, fontsize="xx-large")
+        h5f = tb.open_file(data_path + h5f_name, mode="a")
         try:
-            plt.get_current_fig_manager().window.showMaximized()
-        except Exception:
-            pass
-        plt.show()
-        plt.close("all")
+            current_count = h5f.get_node_attr("/", "count")
+        except AttributeError:
+            current_count = 0
+            h5f.set_node_attr("/", "count", current_count)
+            h5f.set_node_attr("/", "numx", self.numx)
+            h5f.set_node_attr("/", "numy", self.numy)
+            h5f.set_node_attr("/", "alpha", alpha)
+            h5f.set_node_attr("/", "beta", beta)
 
-    def MinimizeGeneralSpinConfig(self, model_params):
-        initial_angles = np.random.random(2 * self.site_num)
-        bounds = [(0, 1), ] * 2 * self.site_num
-        res = minimize(
-            self.GeneralSpinConfig, initial_angles,
-            args=(model_params, ), bounds=bounds
+        spin_vectors_carray = h5f.create_carray(
+            "/", "Run{0:0>4d}".format(current_count + 1),
+            obj=optimized_spin_vectors,
         )
-        if res.success:
-            phis = np.pi * res.x[0::2]
-            thetas = 2 * np.pi * res.x[1::2]
-            xs = np.sin(phis) * np.sin(thetas)
-            ys = np.sin(phis) * np.cos(thetas)
-            zs = np.cos(phis)
-            spin_vectors = np.stack([xs, ys, zs]).T
-            title = "E={0:.8f}".format(res.fun)
-            self.Show3DVectorField(spin_vectors, title=title)
-        else:
-            print("Failed to find a local minimum!")
+        h5f.set_node_attr(spin_vectors_carray, "energy", energy_per_site)
+        h5f.set_node_attr(spin_vectors_carray, "message", res.message)
+        h5f.set_node_attr("/", "count", current_count + 1)
+        h5f.close()
 
-    def MimimizeFMConfig(self, model_params):
-        initial_angle = np.random.random(2)
-        bounds = [(0, 1), (0, 1)]
-        res = minimize(
-            self.FMConfig, initial_angle,
-            args=(model_params, ), bounds=bounds
-        )
-        title_temp = r"E={0:.8f}, $\alpha={1:.4f}\pi$, $\beta={2:.4f}\pi$"
-        if res.success:
-            phi = np.pi * res.x[0]
-            theta = 2 * np.pi * res.x[1]
-            spin_vectors = np.zeros((self.site_num, 3), dtype=np.float64)
-            spin_vectors[:, 0] = np.sin(phi) * np.sin(theta)
-            spin_vectors[:, 1] = np.sin(phi) * np.cos(theta)
-            spin_vectors[:, 2] = np.cos(phi)
-            title = title_temp.format(res.fun, res.x[0], 2 * res.x[1])
-            self.Show3DVectorField(spin_vectors, title=title)
-        else:
-            print("Failed to find a local minimum!")
-
-    def FMConfigEnergyVsDirection(self, model_params, mesh=200):
-        x_bond_num = len(self._x_bonds)
-        y_bond_num = len(self._y_bonds)
-        z_bond_num = len(self._z_bonds)
-        Hijx, Hijy, Hijz = self._HijGenerator(model_params)
-        H = x_bond_num * Hijx + y_bond_num * Hijy + z_bond_num * Hijz
-
-        phis = np.linspace(0, 1, num=mesh)
-        thetas = np.linspace(0, 2, num=2*mesh)
-        mesh_phis, mesh_thetas = np.meshgrid(phis, thetas)
-        mesh_xs = np.sin(mesh_phis * np.pi) * np.sin(mesh_thetas * np.pi)
-        mesh_ys = np.sin(mesh_phis * np.pi) * np.cos(mesh_thetas * np.pi)
-        mesh_zs = np.cos(mesh_phis * np.pi)
-        mesh_vectors = np.dstack([mesh_xs, mesh_ys, mesh_zs])
-        mesh_energies = np.sum(mesh_vectors * np.dot(mesh_vectors, H), axis=-1)
-        del mesh_xs, mesh_ys, mesh_zs, mesh_vectors
-
-        vmin = np.min(mesh_energies)
-        vmax = np.max(mesh_energies)
-        amax = np.max(np.abs([vmin, vmax]))
-        print("Minimum={0}, Maximum={1}".format(vmin, vmax))
-
-        fig = plt.figure()
-        ax0 = fig.add_subplot(121, projection="3d")
-        surf = ax0.plot_surface(
-            mesh_phis, mesh_thetas, mesh_energies,
-            rcount=100, ccount=100,
-            cmap="seismic",
-            vmin=-amax, vmax=amax,
-        )
-        fig.colorbar(surf, ax=ax0)
-
-        ax1 = fig.add_subplot(122)
-        contour_sets = ax1.contourf(
-            mesh_phis, mesh_thetas, mesh_energies,
-            cmap="seismic", levels=500,
-            vmin=-amax, vmax=amax,
-        )
-        phis_tmp = np.arctan2(
-            1, -np.sin(thetas * np.pi) - np.cos(thetas * np.pi)
-        ) / np.pi
-        ax1.plot(phis_tmp, thetas, ls="dashed")
-        fig.colorbar(contour_sets, ax=ax1)
-
-        try:
-            plt.get_current_fig_manager().window.showMaximized()
-        except Exception:
-            pass
-        plt.show()
-        plt.close("all")
-
-
-if __name__ == "__main__":
-    max_iter = 30
-    numx = numy = 6
-    alpha = 1.0
-    beta = 1.6
-    model_params = {
-        "J": np.sin(alpha * np.pi) * np.sin(beta * np.pi),
-        "K": np.sin(alpha * np.pi) * np.cos(beta * np.pi),
-        "G": np.cos(alpha * np.pi),
-        "GP": 0.0,
-    }
-
-    solver = ClassicalJKGGPModel(numx=numx, numy=numy)
-    solver.FMConfigEnergyVsDirection(model_params)
-
-    for i in range(max_iter):
-        solver.MimimizeFMConfig(model_params)
-    for i in range(max_iter):
-        solver.MinimizeGeneralSpinConfig(model_params)
+        if show:
+            fig = plt.figure("OptimizedSpinConfigure")
+            ax = fig.add_subplot(111, projection="3d")
+            title = "E={0}\n".format(energy_per_site)
+            title += r"$\alpha={0:.4f}\pi,\beta={1:.4f}\pi$".format(alpha, beta)
+            ShowVectorField3D(
+                ax, self.cluster.points, optimized_spin_vectors,
+                title=title, **kwargs
+            )
+            ax.set_zlim(-0.5, 0.5)
+            try:
+                plt.get_current_fig_manager().window.showMaximized()
+            except Exception:
+                pass
+            plt.show()
+            plt.close("all")
--- classical/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Analysing the classical J-K-Gamma-Gamma'(J-K-G-GP) model.
-
-The spins are viewed as unit-vectors in 3-dimension.
-"""
