dd0a44bb10eff9749aeb403892a093f60075dafb
wangshiphys <wangshiphys@gmail.com>
2020-04-06 10:31:55 +0800

    Modified `TestFMDirection.py` and `TestStripeDirection.py` according to the newly added `MomentDirection.py` module.

--- run/TestFMDirection.py
+++ run/TestFMDirection.py
@@ -1,113 +1,68 @@
 import logging
 import sys
+from pathlib import Path
 from time import time
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-from utilities import mykron0
+from MomentDirection import FMDirection
 
+logging.basicConfig(
+    stream=sys.stdout, level=logging.INFO, format="%(asctime)s - %(message)s",
+)
+logging.info("Program start running")
 
-def ProbabilitiesOnFM24(thetas, phis, ket):
-    num_thetas = thetas.shape[0]
-    num_phis = phis.shape[0]
-    sin_half_thetas = np.sin(thetas / 2)
-    cos_half_thetas = np.cos(thetas / 2)
-    exp_1j_half_phis = np.exp(1j * phis / 2)
-    exp_1j_half_phis_conj = exp_1j_half_phis.conjugate()
-    probabilities = np.zeros((num_thetas, num_phis), dtype=np.float64)
+PS_DATA_PATH = "TestFM/"
+Path(PS_DATA_PATH).mkdir(exist_ok=True)
+ES_DATA_PATH = "C:/Users/swang/Desktop/JKGModel/data/QuantumSpinModel/ES/"
+ES_DATA_NAME_TEMP = "ES_num1={0}_num2={1}_which={2}_alpha={3:.4f}_beta={4:.4f}.npz"
+PS_DATA_NAME_TEMP = "PS_num1={0}_num2={1}_which={2}_alpha={3:.4f}_beta={4:.4f}.npz"
 
-    spinor2 = np.zeros(2 ** 2, dtype=np.complex128)
-    spinor4 = np.zeros(2 ** 4, dtype=np.complex128)
-    spinor8 = np.zeros(2 ** 8, dtype=np.complex128)
-    spinor16 = np.zeros(2 ** 16, dtype=np.complex128)
-    spinor24 = np.zeros(2 ** 24, dtype=np.complex128)
-    for i in range(num_thetas):
-        t0 = time()
-        for j in range(num_phis):
-            spinor1 = np.array(
-                [
-                    cos_half_thetas[i] * exp_1j_half_phis_conj[j],
-                    sin_half_thetas[i] * exp_1j_half_phis[j],
-                ], dtype=np.complex128,
-            )
+# Prepare thetas and phis
+step = 0.01
+thetas = np.pi * np.arange(0.0, 1.0 + step, step)
+phis = np.pi * np.arange(0.0, 2.0 + step, step)
 
-            mykron0(spinor1, spinor1, spinor2)
-            mykron0(spinor2, spinor2, spinor4)
-            mykron0(spinor4, spinor4, spinor8)
-            mykron0(spinor8, spinor8, spinor16)
-            mykron0(spinor8, spinor16, spinor24)
+num1 = 4
+num2 = 6
+alpha = 0.30
+beta = 1.30
+which = "xy"
+es_data_name = ES_DATA_NAME_TEMP.format(num1, num2, which, alpha, beta)
+ps_data_name = PS_DATA_NAME_TEMP.format(num1, num2, which, alpha, beta)
+ps_fig_name = ps_data_name.replace("npz", "png")
 
-            inner = np.vdot(spinor24, ket)
-            probabilities[i, j] = (inner * inner.conjugate()).real
-        t1 = time()
-        logging.info("%03dth theta, dt=%.4fs", i, t1 - t0)
-    return probabilities
+with np.load(ES_DATA_PATH + es_data_name) as ld:
+    ket = ld["vectors"][:, 0]
 
+t0 = time()
+probabilities = FMDirection(thetas, phis, ket, num1*num2)
+np.savez(
+    PS_DATA_PATH + ps_data_name,
+    size=[num1, num2], which=[which], parameters=[alpha, beta],
+    thetas=thetas, phis=phis, probabilities=probabilities,
+)
+t1 = time()
+logging.info(
+    "Total time spend on alpha=%.3f, beta=%.3f: %.4fs", alpha, beta, t1 - t0
+)
 
-if __name__ == "__main__":
-    logging.basicConfig(
-        stream=sys.stdout, level=logging.INFO,
-        format="%(asctime)s - %(message)s",
-    )
-    logging.info("Program start running")
+fig, ax = plt.subplots()
+cs = ax.pcolormesh(
+    phis / np.pi, thetas / np.pi, probabilities,
+    zorder=0, cmap="magma", shading="gouraud",
+)
+colorbar = fig.colorbar(cs, ax=ax)
 
-    # GS_DATA_PATH = "data/QuantumSpinModel/GS/"
-    GS_DATA_PATH = "E:/JKGModel/data/QuantumSpinModel/GS/"
-    # GS_DATA_PATH = "C:/Users/swang/Desktop/Eigenstates/"
-    PS_DATA_PATH = ""
-    GS_DATA_NAME_TEMP = "GS_numx={0}_numy={1}_alpha={2:.4f}_beta={3:.4f}.npz"
-    PS_DATA_NAME_TEMP = "PS_numx={0}_numy={1}_alpha={2:.4f}_beta={3:.4f}.npz"
-    ############################################################################
+ax.set_aspect("equal")
+ax.grid(True, ls="dashed", color="gray")
+ax.set_yticks([0.0, 0.2, 0.4, 0.5, 0.6, 0.8, 1.0])
+ax.set_xticks([0.0, 0.25, 0.50, 0.75, 1.00, 1.25, 1.50, 1.75, 2.00])
+ax.set_title(r"$\alpha={0:.2f}\pi,\beta={1:.2f}\pi$".format(alpha, beta))
+plt.get_current_fig_manager().window.showMaximized()
+plt.show()
+fig.savefig(PS_DATA_PATH + ps_fig_name, dpi=200, transparent=True)
+plt.close("all")
 
-    # Prepare thetas and phis
-    step = 0.01
-    thetas = np.pi * np.arange(0.0, 1.0 + step, step)
-    phis = np.pi * np.arange(0.0, 2.0 + step, step)
-
-    numx = 4
-    numy = 6
-    alpha = 0.35
-    beta  = 1.63
-    gs_data_name = GS_DATA_NAME_TEMP.format(numx, numy, alpha, beta)
-    ps_data_name = PS_DATA_NAME_TEMP.format(numx, numy, alpha, beta)
-
-    # Load ground state data
-    with np.load(GS_DATA_PATH + gs_data_name) as ld:
-        ket = ld["ket"][:, 0]
-
-    # Calculate and save probabilities data
-    t0 = time()
-    probabilities = ProbabilitiesOnFM24(thetas, phis, ket)
-    np.savez(
-        PS_DATA_PATH + ps_data_name,
-        size=[numx, numy], parameters=[alpha, beta],
-        thetas=thetas, phis=phis, probabilities=probabilities,
-        )
-    t1 = time()
-    logging.info(
-        "The total time spend on alpha=%.3f, beta=%.3f: %.4fs",
-        alpha, beta, t1 - t0
-    )
-
-    # Load overlaps data
-    with np.load(PS_DATA_PATH + ps_data_name) as ld:
-        probabilities = ld["probabilities"]
-
-    fig, ax = plt.subplots(subplot_kw={"polar": True})
-    cs = ax.pcolormesh(
-        phis, thetas, probabilities, zorder=0, cmap="magma", shading="gouraud",
-    )
-    colorbar = fig.colorbar(cs, ax=ax)
-
-    ax.plot(phis, np.arctan2(1, -np.sin(phis) - np.cos(phis)), ls="dashed")
-    ax.plot(np.arctan2(1, 1), np.arctan2(np.sqrt(2), 1), ls="", marker="o")
-
-    ax.set_aspect("equal")
-    ax.grid(True, ls="dashed", color="gray")
-    ax.set_title(r"$\alpha={0:.2f}\pi,\beta={1:.2f}\pi$".format(alpha, beta))
-    plt.get_current_fig_manager().window.showMaximized()
-    plt.show()
-    plt.close("all")
-
-    logging.info("Program stop running")
+logging.info("Program stop running")
--- run/TestStripeDirection.py
+++ run/TestStripeDirection.py
@@ -1,201 +1,70 @@
 import logging
 import sys
+from pathlib import Path
 from time import time
 
 import matplotlib.pyplot as plt
 import numpy as np
-from utilities import mykron0
 
+from MomentDirection import StripeDirection
 
-def ProbabilitiesOnStripe24(thetas, phis, ket, which="x"):
-    num_thetas = thetas.shape[0]
-    num_phis = phis.shape[0]
-    sin_half_thetas_up = np.sin(thetas / 2)
-    cos_half_thetas_up = np.cos(thetas / 2)
-    exp_1j_half_phis_up = np.exp(1j * phis / 2)
-    exp_1j_half_phis_conj_up = exp_1j_half_phis_up.conjugate()
+logging.basicConfig(
+    stream=sys.stdout, level=logging.INFO, format="%(asctime)s - %(message)s",
+)
+logging.info("Program start running")
 
-    sin_half_thetas_down = np.sin((np.pi - thetas) / 2)
-    cos_half_thetas_down = np.cos((np.pi - thetas) / 2)
-    exp_1j_half_phis_down = np.exp(1j * (phis + np.pi) / 2)
-    exp_1j_half_phis_conj_down = exp_1j_half_phis_down.conjugate()
+PS_DATA_PATH = "TestStripe/"
+Path(PS_DATA_PATH).mkdir(exist_ok=True)
+ES_DATA_PATH = "C:/Users/swang/Desktop/JKGModel/data/QuantumSpinModel/ES/"
+ES_DATA_NAME_TEMP = "ES_num1={0}_num2={1}_which={2}_alpha={3:.4f}_beta={4:.4f}.npz"
+PS_DATA_NAME_TEMP = "PS_num1={0}_num2={1}_which={2}_config={3}_alpha={4:.4f}_beta={5:.4f}.npz"
 
-    probabilities = np.zeros((num_thetas, num_phis), dtype=np.float64)
-    if which == "x":
-        spinor2 = np.zeros(2 ** 2, dtype=np.complex128)
-        spinor4 = np.zeros(2 ** 4, dtype=np.complex128)
-        spinor8 = np.zeros(2 ** 8, dtype=np.complex128)
-        spinor16 = np.zeros(2 ** 16, dtype=np.complex128)
-        spinor24 = np.zeros(2 ** 24, dtype=np.complex128)
-        for i in range(num_thetas):
-            t0 = time()
-            for j in range(num_phis):
-                spinor1_up = np.array(
-                    [
-                        cos_half_thetas_up[i] * exp_1j_half_phis_conj_up[j],
-                        sin_half_thetas_up[i] * exp_1j_half_phis_up[j],
-                    ], dtype=np.complex128,
-                )
-                spinor1_down = np.array(
-                    [
-                        cos_half_thetas_down[i] * exp_1j_half_phis_conj_down[j],
-                        sin_half_thetas_down[i] * exp_1j_half_phis_down[j],
-                    ], dtype=np.complex128,
-                )
-                mykron0(spinor1_down, spinor1_up, spinor2)
-                mykron0(spinor2, spinor2, spinor4)
-                mykron0(spinor4, spinor4, spinor8)
-                mykron0(spinor8, spinor8, spinor16)
-                mykron0(spinor16, spinor8, spinor24)
+# Prepare thetas and phis
+step = 0.01
+thetas = np.pi * np.arange(0.0, 1.0 + step, step)
+phis = np.pi * np.arange(0.0, 2.0 + step, step)
 
-                inner = np.vdot(spinor24, ket)
-                probabilities[i, j] = (inner * inner.conjugate()).real
-            t1 = time()
-            logging.info("%03dth theta, dt=%.4fs", i, t1 - t0)
-    elif which == "y":
-        spinor2_down_up = np.zeros(2 ** 2, dtype=np.complex128)
-        spinor4_down_up = np.zeros(2 ** 4, dtype=np.complex128)
-        spinor6_down_up = np.zeros(2 ** 6, dtype=np.complex128)
-        spinor2_up_down = np.zeros(2 ** 2, dtype=np.complex128)
-        spinor4_up_down = np.zeros(2 ** 4, dtype=np.complex128)
-        spinor6_up_down = np.zeros(2 ** 6, dtype=np.complex128)
-        spinor12 = np.zeros(2 ** 12, dtype=np.complex128)
-        spinor24 = np.zeros(2 ** 24, dtype=np.complex128)
-        for i in range(num_thetas):
-            t0 = time()
-            for j in range(num_phis):
-                spinor1_up = np.array(
-                    [
-                        cos_half_thetas_up[i] * exp_1j_half_phis_conj_up[j],
-                        sin_half_thetas_up[i] * exp_1j_half_phis_up[j],
-                    ], dtype=np.complex128,
-                )
-                spinor1_down = np.array(
-                    [
-                        cos_half_thetas_down[i] * exp_1j_half_phis_conj_down[j],
-                        sin_half_thetas_down[i] * exp_1j_half_phis_down[j],
-                    ], dtype=np.complex128,
-                )
+num1 = 4
+num2 = 6
+alpha = 0.50
+beta = 0.80
+which = "xy"
+config = "StripeX"
+es_data_name = ES_DATA_NAME_TEMP.format(num1, num2, which, alpha, beta)
+ps_data_name = PS_DATA_NAME_TEMP.format(num1, num2, which, config, alpha, beta)
+ps_fig_name = ps_data_name.replace("npz", "png")
 
-                mykron0(spinor1_down, spinor1_up, spinor2_down_up)
-                mykron0(spinor2_down_up, spinor2_down_up, spinor4_down_up)
-                mykron0(spinor4_down_up, spinor2_down_up, spinor6_down_up)
-                mykron0(spinor1_up, spinor1_down, spinor2_up_down)
-                mykron0(spinor2_up_down, spinor2_up_down, spinor4_up_down)
-                mykron0(spinor4_up_down, spinor2_up_down, spinor6_up_down)
-                mykron0(spinor6_down_up, spinor6_up_down, spinor12)
-                mykron0(spinor12, spinor12, spinor24)
+with np.load(ES_DATA_PATH + es_data_name) as ld:
+    ket = ld["vectors"][:, 0]
 
-                inner = np.vdot(spinor24, ket)
-                probabilities[i, j] = (inner * inner.conjugate()).real
-            t1 = time()
-            logging.info("%03dth theta, dt=%.4fs", i, t1 - t0)
-    elif which == "z":
-        spinor2_up = np.zeros(2 ** 2, dtype=np.complex128)
-        spinor4_up = np.zeros(2 ** 4, dtype=np.complex128)
-        spinor6_up = np.zeros(2 ** 6, dtype=np.complex128)
-        spinor2_down = np.zeros(2 ** 2, dtype=np.complex128)
-        spinor4_down = np.zeros(2 ** 4, dtype=np.complex128)
-        spinor6_down = np.zeros(2 ** 6, dtype=np.complex128)
-        spinor12 = np.zeros(2 ** 12, dtype=np.complex128)
-        spinor24 = np.zeros(2 ** 24, dtype=np.complex128)
-        for i in range(num_thetas):
-            t0 = time()
-            for j in range(num_phis):
-                spinor1_up = np.array(
-                    [
-                        cos_half_thetas_up[i] * exp_1j_half_phis_conj_up[j],
-                        sin_half_thetas_up[i] * exp_1j_half_phis_up[j],
-                    ], dtype=np.complex128,
-                )
-                spinor1_down = np.array(
-                    [
-                        cos_half_thetas_down[i] * exp_1j_half_phis_conj_down[j],
-                        sin_half_thetas_down[i] * exp_1j_half_phis_down[j],
-                    ], dtype=np.complex128,
-                )
+t0 = time()
+probabilities = StripeDirection(
+    thetas, phis, ket, num1=num1, num2=num2, which=which, config=config
+)
+np.savez(
+    PS_DATA_PATH + ps_data_name,
+    size=[num1, num2], which=[which], config=[config], parameters=[alpha, beta],
+    thetas=thetas, phis=phis, probabilities=probabilities,
+)
+t1 = time()
+logging.info(
+    "Total time spend on alpha=%.3f, beta=%.3f: %.4fs", alpha, beta, t1 - t0
+)
 
-                mykron0(spinor1_up, spinor1_up, spinor2_up)
-                mykron0(spinor2_up, spinor2_up, spinor4_up)
-                mykron0(spinor4_up, spinor2_up, spinor6_up)
-                mykron0(spinor1_down, spinor1_down, spinor2_down)
-                mykron0(spinor2_down, spinor2_down, spinor4_down)
-                mykron0(spinor4_down, spinor2_down, spinor6_down)
-                mykron0(spinor6_down, spinor6_up, spinor12)
-                mykron0(spinor12, spinor12, spinor24)
+fig, ax = plt.subplots()
+cs = ax.pcolormesh(
+    phis / np.pi, thetas / np.pi, probabilities,
+    zorder=0, cmap="magma", shading="gouraud",
+)
+colorbar = fig.colorbar(cs, ax=ax)
 
-                inner = np.vdot(spinor24, ket)
-                probabilities[i, j] = (inner * inner.conjugate()).real
-            t1 = time()
-            logging.info("%03dth theta, dt=%.4fs", i, t1 - t0)
-    else:
-        raise ValueError("Invalid `which` parameter: {0}".format(which))
-    return probabilities
-
-
-if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.INFO,
-        stream=sys.stdout,
-        format="%(asctime)s - %(message)s",
-    )
-    logging.info("Program start running")
-
-
-    PS_DATA_PATH = ""
-    # GS_DATA_PATH = "C:/Users/swang/Desktop/Eigenstates/"
-    GS_DATA_PATH = "E:/JKGModel/data/QuantumSpinModel/GS/"
-    GS_DATA_NAME_TEMP = "GS_numx={0}_numy={1}_alpha={2:.4f}_beta={3:.4f}.npz"
-    PS_DATA_NAME_TEMP = "PS_numx={0}_numy={1}_" \
-                        "alpha={2:.4f}_beta={3:.4f}_which={4}.npz"
-    ############################################################################
-
-    # Prepare thetas and phis
-    step = 0.01
-    thetas = np.pi * np.arange(0.0, 1.0 + step, step)
-    phis = np.pi * np.arange(0.0, 2.0 + step, step)
-
-    numx = 4
-    numy = 6
-    alpha = 0.30
-    beta  = 0.27
-    which = "x"
-    gs_data_name = GS_DATA_NAME_TEMP.format(numx, numy, alpha, beta)
-    ps_data_name = PS_DATA_NAME_TEMP.format(numx, numy, alpha, beta, which)
-
-    # Load ground state data
-    with np.load(GS_DATA_PATH + gs_data_name) as ld:
-        ket = ld["ket"][:, 0]
-
-    # Calculate and save probabilities data
-    t0 = time()
-    probabilities = ProbabilitiesOnStripe24(thetas, phis, ket, which=which)
-    np.savez(
-        PS_DATA_PATH + ps_data_name,
-        size=[numx, numy], parameters=[alpha, beta], which=[which],
-        thetas=thetas, phis=phis, probabilities=probabilities,
-    )
-    t1 = time()
-    logging.info(
-        "The total time spend on alpha=%.3f, beta=%.3f: %.4fs",
-        alpha, beta, t1 - t0
-    )
-
-    # Load overlaps data
-    with np.load(PS_DATA_PATH + ps_data_name) as ld:
-        probabilities = ld["probabilities"]
-
-    fig, ax = plt.subplots(subplot_kw={"polar": True})
-    cs = ax.pcolormesh(
-        phis, thetas, probabilities, zorder=0, cmap="magma", shading="gouraud",
-    )
-    colorbar = fig.colorbar(cs, ax=ax)
-
-    ax.set_aspect("equal")
-    ax.grid(True, ls="dashed", color="gray")
-    ax.set_title(r"$\alpha={0:.2f}\pi,\beta={1:.2f}\pi$".format(alpha, beta))
-    plt.get_current_fig_manager().window.showMaximized()
-    plt.show()
-    plt.close("all")
-
-    logging.info("Program stop running")
+ax.set_aspect("equal")
+ax.grid(True, ls="dashed", color="gray")
+ax.set_yticks([0.0, 0.2, 0.4, 0.5, 0.6, 0.8, 1.0])
+ax.set_xticks([0.0, 0.25, 0.50, 0.75, 1.00, 1.25, 1.50, 1.75, 2.00])
+ax.set_title(r"$\alpha={0:.2f}\pi,\beta={1:.2f}\pi$".format(alpha, beta))
+plt.get_current_fig_manager().window.showMaximized()
+plt.show()
+fig.savefig(PS_DATA_PATH + ps_fig_name, dpi=100, transparent=True)
+plt.close("all")
+logging.info("Program stop running")
