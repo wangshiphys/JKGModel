805622e58100342bf6d1828317bd623d74d4c19f
wangshiphys <wangshiphys@gmail.com>
2020-06-01 20:21:46 +0800

    Some minor changes to `MomentDirection.py`.

--- MomentDirection.py
+++ MomentDirection.py
@@ -1,9 +1,18 @@
+"""
+Test the moment direction of a magnetic ordered state.
+"""
+
+__all__ = ["FMDirection", "StripeDirection"]
+
+
 import logging
 from time import time
 
 import numpy as np
 from HamiltonianPy import Lattice
 from numba import complex128, jit
+
+from utilities import TriangularLattice
 
 
 @jit(complex128[:](complex128[:], complex128[:]), nopython=True, cache=True)
@@ -54,32 +63,29 @@ def multikron(vectors):
         return mykron(left, right)
 
 
-def ClusterGenerator(num1, num2=None, which="xy"):
-    assert isinstance(num1, int) and num1 > 0
-    assert (num2 is None) or (isinstance(num2, int) and num2 > 0)
-    assert which in ("xy", "yz", "zx", "yx", "zy", "xz")
-    if num2 is None:
-        num2 = num1
-
-    RX = np.array([1.0, 0.0], dtype=np.float64)
-    RY = np.array([-0.5, np.sqrt(3) / 2], dtype=np.float64)
-    RZ = np.array([0.5, np.sqrt(3) / 2], dtype=np.float64)
-    AS = {
-        "xy": np.array([RX, RY], dtype=np.float64),
-        "yz": np.array([RY, RZ], dtype=np.float64),
-        "zx": np.array([RZ, RX], dtype=np.float64),
-        "yx": np.array([RY, RX], dtype=np.float64),
-        "zy": np.array([RZ, RY], dtype=np.float64),
-        "xz": np.array([RX, RZ], dtype=np.float64),
-    }
-
-    As = AS[which]
-    vectors = As * np.array([[num1], [num2]])
-    points = np.dot([[i, j] for i in range(num1) for j in range(num2)], As)
-    return Lattice(points=points, vectors=vectors, name=which)
-
-
 def StripeGenerator(cluster, config="StripeX"):
+    """
+    Generate stripe ordered state on the given cluster.
+
+    Parameters
+    ----------
+    cluster : Lattice
+        The cluster on which to generate the stripe ordered state.
+    config : ["StripeX" | "StripeY", | "StripeZ"], str, optional
+        The type of the stripe order to generate.
+        If set to "StripeX", the spins along the x-bond direction are parallel;
+        If set to "StripeY", the spins along the y-bond direction are parallel;
+        If set to "StripeZ", the spins along the z-bond direction are parallel.
+        Default: "StripeX".
+
+    Returns
+    -------
+    spin_up_indices : list of int
+        The indices of the lattice sites which are in spin-up state.
+    spin_down_indices : list of int
+        The indices of the lattice sites which are in spin-down state.
+    """
+
     if config == "StripeX":
         points = np.array([[0.0, 0.0], [0.5, np.sqrt(3) / 2]])
         vectors = np.array([[1.0, 0.0], [1.0, np.sqrt(3)]])
@@ -90,22 +96,47 @@ def StripeGenerator(cluster, config="StripeX"):
         points = np.array([[0.0, 0.0], [1.0, 0.0]])
         vectors = np.array([[2.0, 0.0], [0.5, np.sqrt(3) / 2]])
     else:
-        raise ValueError("Invalid config: {0}".format(config))
+        raise ValueError("Invalid `config`: {0}".format(config))
     cell = Lattice(points, vectors)
 
-    up_indices = []
-    down_indices = []
+    spin_up_indices = []
+    spin_down_indices = []
     for point in cluster.points:
         index_cell = cell.getIndex(point, fold=True)
         index_cluster = cluster.getIndex(point, fold=False)
         if index_cell == 0:
-            up_indices.append(index_cluster)
+            spin_up_indices.append(index_cluster)
         else:
-            down_indices.append(index_cluster)
-    return up_indices, down_indices
+            spin_down_indices.append(index_cluster)
+    return spin_up_indices, spin_down_indices
 
 
 def FMDirection(thetas, phis, ket, site_num):
+    """
+    The probabilities of the given `ket` in different polarized FM states.
+
+    thetas : 1D array
+        A collection of radial angles.
+        The radial angle is the angle between the moment direction and the
+        z-axis. It should be in the range [0, pi].
+    phis : 1D array
+        A collection of azimuth angles.
+        The azimuth angle is the angle between the projection of the moment
+        direction on the xy-plane and the x-axis. It should be in the
+        range [0, 2*pi). `theta` and `phi` specify the direction of the
+        ordered moment. The given `thetas` and `phis` specify a mesh of
+        different directions.
+    site_num : int
+        The number of lattice sites of the system.
+    ket : 1D array with shape (2 ** site_num, )
+        Matrix representation of the quantum state.
+
+    Return
+    ------
+    probabilities : 2D array
+        The probabilities of the `ket` in these polarized FM states.
+    """
+
     num_phis = phis.shape[0]
     num_thetas = thetas.shape[0]
     sin_half_thetas = np.sin(thetas / 2)
@@ -125,15 +156,47 @@ def FMDirection(thetas, phis, ket, site_num):
             inner = np.vdot(multikron(spinors), ket)
             probabilities[i, j] = (inner * inner.conjugate()).real
         t1 = time()
-        logging.info("%03dth theta, dt=%.4fs", i, t1 - t0)
+        logging.info("%03dth theta, dt=%.3fs", i, t1 - t0)
     return probabilities
 
 
 def StripeDirection(
-        thetas, phis, ket, num1, num2=None, which="xy", config="StripeX"
+        thetas, phis, ket, *, num1=4, num2=6, direction="xy", config="StripeX"
 ):
-    cluster = ClusterGenerator(num1, num2, which)
-    up_indices, down_indices = StripeGenerator(cluster, config=config)
+    """
+    The probabilities of the given `ket` in different polarized stripe states.
+
+    thetas : 1D array
+        A collection of radial angles.
+        The radial angle is the angle between the moment direction and the
+        z-axis. It should be in the range [0, pi].
+    phis : 1D array
+        A collection of azimuth angles.
+        The azimuth angle is the angle between the projection of the moment
+        direction on the xy-plane and the x-axis. It should be in the
+        range [0, 2*pi). `theta` and `phi` specify the direction of the
+        ordered moment. The given `thetas` and `phis` specify a mesh of
+        different directions.
+    ket : 1D array with shape (2 ** site_num, )
+        Matrix representation of the quantum state.
+    config : ["StripeX" | "StripeY", | "StripeZ"], str, optional
+        Determine the type of the stripe order.
+        If set to "StripeX", the spins along the x-bond direction are parallel;
+        If set to "StripeY", the spins along the y-bond direction are parallel;
+        If set to "StripeZ", the spins along the z-bond direction are parallel.
+        Default: "StripeX".
+    num1, num2, direction
+        They are passed to the constructor of `TriangularLattice` defined in
+        `utilities` module. See also the document of `TriangularLattice`.
+
+    Return
+    ------
+    probabilities : 2D array
+        The probabilities of the `ket` in these polarized stripe states.
+    """
+
+    cluster = TriangularLattice(num1, num2, direction).cluster
+    spin_up_indices, spin_down_indices = StripeGenerator(cluster, config=config)
 
     # The opposite direction to (theta, phi) is (pi - theta, phi + pi)
     num_phis = phis.shape[0]
@@ -156,14 +219,14 @@ def StripeDirection(
             tmp1 = sin_half_thetas_up[i] * exp_1j_half_phis_up[j]
             tmp2 = cos_half_thetas_down[i] * exp_1j_half_phis_conj_down[j]
             tmp3 = sin_half_thetas_down[i] * exp_1j_half_phis_down[j]
-            spinors[up_indices, 0] = tmp0
-            spinors[up_indices, 1] = tmp1
-            spinors[down_indices, 0] = tmp2
-            spinors[down_indices, 1] = tmp3
+            spinors[spin_up_indices, 0] = tmp0
+            spinors[spin_up_indices, 1] = tmp1
+            spinors[spin_down_indices, 0] = tmp2
+            spinors[spin_down_indices, 1] = tmp3
 
             # cluster_spin_coherent_state = multikron(spinors)
             inner = np.vdot(multikron(spinors), ket)
             probabilities[i, j] = (inner * inner.conjugate()).real
         t1 = time()
-        logging.info("%03dth theta, dt=%.4fs", i, t1 - t0)
+        logging.info("%03dth theta, dt=%.3fs", i, t1 - t0)
     return probabilities
