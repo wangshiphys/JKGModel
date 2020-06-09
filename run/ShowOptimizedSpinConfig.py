35934105aeefc5506026808bb731cedf80080a28
wangshiphys <wangshiphys@gmail.com>
2020-06-09 13:33:41 +0800

    Minor changes to `ShowOptimizedSpinConfig.py`.

--- run/ShowOptimizedSpinConfig.py
+++ run/ShowOptimizedSpinConfig.py
@@ -1,39 +1,38 @@
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import tables as tb
 
-from HamiltonianPy import lattice_generator
-from utilities import ShowVectorField3D
-
+from utilities import TriangularLattice
 
 data_path = "data/ClassicalSpinModel/OptimizedSpinConfig/"
-h5f_name_temp = "OSC_numx={0}_numy={1}_alpha={2:.4f}_beta={3:.4f}.h5"
+h5f_name_temp = "OSC_num1={0}_num2={1}_direction={2}_" \
+                "alpha={3:.4f}_beta={4:.4f}.h5"
 
-alpha = 0.5
-beta = 1.85
-numx = numy = 12
-cluster = lattice_generator("triangle", num0=numx, num1=numy)
+alpha = 0.50
+beta = 0.50
+num1 = num2 = 12
+direction = "xz"
+lattice = TriangularLattice(num1=num1, num2=num2, direction="xz")
+points = lattice.cluster.points
 
-h5f_name = h5f_name_temp.format(numx, numy, alpha, beta)
+h5f_name = h5f_name_temp.format(num1, num2, direction, alpha, beta)
 h5f = tb.open_file(data_path + h5f_name, mode="r")
 for carray in h5f.iter_nodes("/"):
-    energy = h5f.get_node_attr(carray, "energy")
     spin_vectors = carray.read()
-    print(spin_vectors)
+    energy = h5f.get_node_attr(carray, "energy")
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection="3d")
-    title = "{0}\n".format(carray.name)
-    title += r"$\alpha={0:.4f}\pi,\beta={1:.4f}\pi,E_{{min}}={2:.8f}$".format(
-        alpha, beta, energy
+    fig, ax = plt.subplots(num=carray.name)
+    ax.plot(
+        points[:, 0], points[:, 1],
+        color="k", ls="", marker="o", ms=5
     )
-    ShowVectorField3D(
-        ax, cluster.points, spin_vectors, title=title
+    ax.quiver(
+        points[:, 0], points[:, 1],
+        spin_vectors[:, 0], spin_vectors[:, 1],
+        pivot="middle", color=0.5*spin_vectors+0.5,
     )
-    ax.set_zlim(-0.5, 0.5)
-    ax.set_xlabel("X")
-    ax.set_ylabel("Y")
-    ax.set_zlabel("Z")
+    ax.set_title("Energy = {0:.10f}".format(energy), fontsize="xx-large")
+    ax.set_axis_off()
+    ax.set_aspect("equal")
     plt.get_current_fig_manager().window.showMaximized()
     plt.show()
     plt.close("all")
