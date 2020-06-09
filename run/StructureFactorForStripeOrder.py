4188ca134000ddef4e89ed2f6483d2d9296110b5
wangshiphys <wangshiphys@gmail.com>
2020-06-09 21:28:13 +0800

    Minor changes to `StructureFactorForStripeOrder.py`.

--- run/StructureFactorForStripeOrder.py
+++ run/StructureFactorForStripeOrder.py
@@ -1,92 +1,71 @@
-from time import time
-
+import HamiltonianPy as HP
 import matplotlib.pyplot as plt
 import numpy as np
-from HamiltonianPy import lattice_generator, Lattice
-from HamiltonianPy import TRIANGLE_CELL_KS
 
 from StructureFactor import ClassicalSpinStructureFactor
-from utilities import ShowVectorField3D
 
-
-bs = 4 * np.pi * np.identity(2) / np.sqrt(3)
-step = 0.01
-kx_ratios = np.arange(-0.7, 0.7 + step, step)
-ky_ratios = np.arange(-0.7 + step, 0.7, step)
-kpoints = np.matmul(
-    np.stack(np.meshgrid(kx_ratios, ky_ratios, indexing="ij"), axis=-1), bs
-)
-BZBoundary = TRIANGLE_CELL_KS[[*range(6), 0]]
-
-numx = numy = 8
-cluster = lattice_generator("triangle", num0=numx, num1=numy)
-
-which = "z"
-if which == "x":
-    # FM along x-bond
+num =  30
+config = "StripeZ"
+if config == "StripeX":
+    # FM along x-bond direction
     cell_points = np.array([[0, 0], [0.5, np.sqrt(3)/2]], dtype=np.float64)
     cell_vectors = np.array([[1, 0], [1, np.sqrt(3)]], dtype=np.float64)
-elif which == "y":
-    # FM along y-bond
+elif config == "StripeY":
+    # FM along y-bond direction
     cell_points = np.array([[0, 0], [1, 0]], dtype=np.float64)
     cell_vectors = np.array([[2, 0], [-0.5, np.sqrt(3)/2]], dtype=np.float64)
-elif which == "z":
-    # FM along z-bond
+elif config == "StripeZ":
+    # FM along z-bond direction
     cell_points = np.array([[0, 0], [1, 0]], dtype=np.float64)
     cell_vectors = np.array([[2, 0], [0.5, np.sqrt(3)/2]], dtype=np.float64)
 else:
-    raise ValueError("Invalid `which` variable!")
-cell = Lattice(points=cell_points, vectors=cell_vectors)
+    raise ValueError("Invalid `config`: {0}".format(config))
+cell = HP.Lattice(points=cell_points, vectors=cell_vectors)
+cluster = HP.lattice_generator("triangle", num0=num, num1=num)
 
-theta = np.pi * np.random.random()
-phi = 2 * np.pi * np.random.random()
-vx = np.sin(theta) * np.cos(phi)
-vy = np.sin(theta) * np.sin(phi)
-vz = np.cos(theta)
+tmp = 2 * np.random.random(3) - 1
+vx, vy, vz = tmp / np.linalg.norm(tmp)
 spin_vectors = []
 for point in cluster.points:
     index = cell.getIndex(site=point, fold=True)
     if index == 0:
         spin_vectors.append([vx, vy, vz])
-    elif index == 1:
-        spin_vectors.append([-vx, -vy, -vz])
     else:
-        raise RuntimeError("Something is wrong in `cell.getIndex`!")
+        spin_vectors.append([-vx, -vy, -vz])
 spin_vectors = np.array(spin_vectors, dtype=np.float64)
 
-t0 = time()
+step = 0.01
+ratios = np.arange(-0.7, 0.7 + step, step)
+kpoints = np.matmul(
+    np.stack(np.meshgrid(ratios, ratios, indexing="ij"), axis=-1),
+    4 * np.pi * np.identity(2) / np.sqrt(3)
+)
+BZBoundary = HP.TRIANGLE_CELL_KS[[*range(6), 0]]
+
 factors = ClassicalSpinStructureFactor(kpoints, cluster.points, spin_vectors)
 assert np.all(np.abs(factors.imag) < 1E-12)
-t1 = time()
-print("The time spend: {0:.4f}s".format(t1 - t0))
+factors = factors.real
 
-fig0 = plt.figure("original")
-ax0 = fig0.add_subplot(111, projection="3d")
-ShowVectorField3D(ax0, cluster.points, spin_vectors)
-ax0.set_xlabel("X", fontsize="xx-large")
-ax0.set_ylabel("Y", fontsize="xx-large")
-ax0.set_zlabel("Z", fontsize="xx-large")
-ax0.set_zlim(-0.5, 0.5)
-
-fig1, ax1 = plt.subplots()
-im1 = ax1.pcolormesh(
-    kpoints[:, :, 0], kpoints[:, :, 1], factors.real,
+fig, ax = plt.subplots(num=config)
+im = ax.pcolormesh(
+    kpoints[:, :, 0], kpoints[:, :, 1], factors, zorder=0,
     cmap="magma", shading="gouraud",
 )
-fig1.colorbar(im1, ax=ax1)
-ax1.plot(
+fig.colorbar(im, ax=ax)
+ax.plot(
     BZBoundary[:, 0], BZBoundary[:, 1], zorder=1,
     lw=3, ls="dashed", color="tab:blue", alpha=1.0,
 )
-ticks = np.array([-1, 0, 1], dtype=np.int64)
-ax1.set_xticks(ticks * np.pi)
-ax1.set_yticks(ticks * np.pi)
-ax1.set_xticklabels(["{0}".format(tick) for tick in ticks])
-ax1.set_yticklabels(["{0}".format(tick) for tick in ticks])
-ax1.set_xlabel(r"$k_x/\pi$", fontsize="large")
-ax1.set_ylabel(r"$k_y/\pi$", fontsize="large")
-ax1.grid(True, ls="dashed", color="gray")
-ax1.set_aspect("equal")
+ticks = np.array([-1, 0, 1])
+ax.set_xticks(ticks * np.pi)
+ax.set_yticks(ticks * np.pi)
+ax.set_xticklabels(["{0}".format(tick) for tick in ticks])
+ax.set_yticklabels(["{0}".format(tick) for tick in ticks])
+ax.set_xlabel(r"$k_x/\pi$", fontsize="large")
+ax.set_ylabel(r"$k_y/\pi$", fontsize="large")
+ax.grid(True, ls="dashed", color="gray")
+ax.set_aspect("equal")
 plt.get_current_fig_manager().window.showMaximized()
+plt.tight_layout()
 plt.show()
 plt.close("all")
