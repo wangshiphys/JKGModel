5cebe368b83ed9b9a7284eca938fb9c869bf7815
wangshiphys <wangshiphys@gmail.com>
2020-07-23 15:26:23 +0800

    Add `FontSize.py`.

---
+++ FiguresForPaper/FontSize.py
@@ -0,0 +1,24 @@
+"""
+Font size used in this package.
+"""
+
+
+__all__ = [
+    "XXSMALL", "XSMALL", "SMALLER",
+    "SMALL", "MEDIUM", "LARGE",
+    "LARGER", "XLARGE", "XXLARGE",
+]
+
+
+MEDIUM = 20
+
+SMALL = MEDIUM - 2
+XSMALL = SMALL - 2
+XXSMALL = XSMALL - 2
+
+LARGE = MEDIUM + 2
+XLARGE = LARGE + 2
+XXLARGE = XLARGE + 2
+
+LARGER = XLARGE
+SMALLER = XSMALL
