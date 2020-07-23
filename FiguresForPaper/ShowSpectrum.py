24a1d8e19b688b9497cbadcdfd1d9742041053a6
wangshiphys <wangshiphys@gmail.com>
2020-07-23 15:30:12 +0800

    Minor changes to `ShowSpectrum.py`.

--- FiguresForPaper/ShowSpectrum.py
+++ FiguresForPaper/ShowSpectrum.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from FontSize import *
+
 data_name = "data/Spectrum_num1=4_num2=6_direction=xy_" \
             "alpha=0.0500_beta=0.1000_excitation=Sm.npz"
 
@@ -18,19 +20,20 @@ im = ax.pcolormesh(
 )
 im.set_edgecolor("face")
 colorbar = fig.colorbar(im, ax=ax, pad=0.01)
-colorbar.ax.tick_params(axis="y", labelsize="xx-large")
+colorbar.ax.tick_params(axis="y", labelsize=SMALL)
 ax.set_ylim(0, 2)
 ax.grid(ls="dashed")
 ax.set_yticks([0.0, 0.5, 1.0, 1.5, 2.0])
 ax.set_xticks(np.arange(kpoint_num) + 0.5)
 ytick_labels = ["0.0", "0.5", "1.0", "1.5", "2.0"]
-xtick_labels = ["$M$", "", "", r"$\Gamma$", "", "", "$M$", "", "", r"$\Gamma$"]
-ax.set_xticklabels(xtick_labels, fontsize="xx-large")
-ax.set_yticklabels(ytick_labels, fontsize="xx-large")
+xtick_labels = [
+    "$\~M$", "", "", r"$\~\Gamma$", "", "", "$\~M$", "", "",  r"$\~\Gamma$"
+]
+ax.set_xticklabels(xtick_labels, fontsize=SMALL)
+ax.set_yticklabels(ytick_labels, fontsize=SMALL)
 
 plt.get_current_fig_manager().window.showMaximized()
 plt.tight_layout()
 plt.show()
-# fig.savefig("figures/Spectrum.pdf", dpi=100, transparent=True)
-# fig.savefig("figures/Spectrum.png", dpi=100, transparent=True)
+fig.savefig("figures/Spectrum.pdf", transparent=True)
 plt.close("all")
