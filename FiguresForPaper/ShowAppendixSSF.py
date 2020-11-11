4245c3f5f92a2f30cb6b25736b7739e863b2813a
wangshiphys <wangshiphys@gmail.com>
2020-11-11 14:48:27 +0800

    Minor changes to `ShowAppendixSSF.py`.

--- FiguresForPaper/ShowAppendixSSF.py
+++ FiguresForPaper/ShowAppendixSSF.py
@@ -5,18 +5,17 @@ parameters.
 
 import matplotlib.pyplot as plt
 import numpy as np
-from HamiltonianPy import TRIANGLE_CELL_MS as MS
 
 from FontSize import *
 
 BZLW = 5
 COLORMAP = "hot_r"
 BZCOLOR = "tab:green"
-SF_DATA_NAME_TEMP = "data/SF_num1=4_num2=6_which=avg_" \
-                    "alpha={0:.4f}_beta={1:.4f}.npz"
+SF_DATA_NAME_TEMP = "data/SF_num1=4_num2=6_" \
+                    "direction=avg_alpha={0:.4f}_beta={1:.4f}.npz"
 
 sub_fig_tags = ["(a)", "(b)", "(c)", "(d)"]
-params = [(0.50, 0.50), (0.50, 1.86), (0.30, 1.30), (0.7, 1.65)]
+params = [(0.50, 0.50), (0.50, 1.86), (0.00, 0.00), (1.00, 0.00)]
 
 fig, axes = plt.subplots(nrows=2, ncols=2, sharex="all", sharey="all")
 for index in range(len(params)):
@@ -39,8 +38,7 @@ for index in range(len(params)):
     )
     im.set_edgecolor("face")
 
-    colorbar = fig.colorbar(im, ax=ax, pad=0.01, format="%.1f")
-    colorbar.ax.tick_params(axis="y", labelsize="large")
+    colorbar = fig.colorbar(im, ax=ax, pad=0.02, format="%.1f")
     colorbar.set_ticks(np.linspace(vmin, vmax, 5, endpoint=True))
     colorbar.ax.tick_params(axis="y", labelsize=SMALL)
 
@@ -48,23 +46,17 @@ for index in range(len(params)):
         BZBoundary[:, 0], BZBoundary[:, 1], zorder=1,
         ls="dashed", lw=BZLW, color=BZCOLOR, alpha=1.0,
     )
+    ax.set_title(sub_fig_tag + " " + title, fontsize=LARGE, loc="center")
+    ax.set_aspect("equal")
+    ax.set_xlim(-4.5, 4.5)
+    ax.set_ylim(-4.5, 4.5)
 
-    ax.text(
-        0.0, 0.98, sub_fig_tag,
-        fontsize=LARGE, ha="left", va="top", transform=ax.transAxes,
-    )
-    ax.set_title(title, fontsize="xx-large", loc="center")
-    ax.set_title(title, fontsize=LARGE, loc="center")
     ticks = np.array([-1, 0, 1], dtype=np.int64)
     tick_labels = ["{0}".format(tick) for tick in ticks]
     ax.set_xticks(ticks * np.pi)
     ax.set_yticks(ticks * np.pi)
-    ax.set_xticklabels(tick_labels, fontsize="large")
-    ax.set_yticklabels(tick_labels, fontsize="large")
     ax.set_xticklabels(tick_labels, fontsize=SMALL)
     ax.set_yticklabels(tick_labels, fontsize=SMALL)
-    ax.grid(True, ls="dashed", color="gray")
-    ax.set_aspect("equal")
 
 axes[1, 0].set_xlabel(r"$k_x/\pi$", fontsize=LARGE)
 axes[1, 1].set_xlabel(r"$k_x/\pi$", fontsize=LARGE)
